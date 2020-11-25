52c7bc758c9d9bb288d791b29c1ed48b9f2bec5c
Craig5117 <craig.bennett5117@gmail.com>
2020-11-25 15:08:08 -0500

    finished the whole site generation

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const fs = require('fs');
+// This is the same as the next line. The second way allows us to simply call writeFile or copyFile without generateSite. before it
+// const generateSite = require('./utils/generate-site.js');
+
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const generatePage = require('./src/page-template.js');
 
@@ -160,14 +163,37 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData)
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-    console.log('Page created! Check out index.html in this directory to see it!');
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-    
-});
+  //   fs.writeFile('./dist/index.html', pageHTML, err => {
+  //     if (err) {
+  //       console.log(err);
+  //       return;
+  //     }
+  //     console.log('Page created! Check out index.html in this directory to see it!');
+      
+  //     fs.copyFile('./src/style.css', './dist/style.css', err => {
+  //       if (err) {
+  //         console.log(err);
+  //         return;
+  //       }
+  //       console.log('Style sheet copied successfully!');
+  //     });
+  //   });    
+  // });
 
 
 
